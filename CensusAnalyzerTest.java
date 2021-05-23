d26fef83c2af436191ead336a7eb92f09f31b78a
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-23 17:15:11 +0530

    ADD:check to ensure the number of record number of record matches

--- CensusAnalyzerTest.java
+++ CensusAnalyzerTest.java
@@ -1,6 +1,17 @@
 package com.day29.IndainStateCensusAnalyser;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
 
-	public class CensusAnalyzerTest 
-	{
-		
-	}
+
+public class CensusAnalyzerTest 
+{
+	final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\Ankita\\eclipse-workspace\\IndainStateCensusAnalyser\\src\\data\\IndiaCensus.csv";
+
+	@Test
+	public void givenIndianCensusCSVFileReturnsCorrectNumOfRecords() throws IOException {
+		CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
+		int numOfRecords = censusAnalyZer.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+		Assert.assertEquals(1, numOfRecords);
+  }
+}
