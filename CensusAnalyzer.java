1d264edcc40ad0ae8f024992ab1d0fe24812402a
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-23 18:50:10 +0530

    Add:census csvfile correct-but delimeter incorrect custom exception

--- CensusAnalyzer.java
+++ CensusAnalyzer.java
@@ -1,18 +1,23 @@
 package com.day29.IndainStateCensusAnalyser;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.stream.StreamSupport;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CensusAnalyzer 
 {
-	public int loadIndiaCensusData(String csvFilePath) throws IOException {
+	public int loadIndiaCensusData(String csvFilePath) throws IOException, CensusAnalyzerException {
 		try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));)  {
 			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 			csvToBeanBuilder.withType(IndiaCensusCSV.class);
@@ -22,15 +27,33 @@ public class CensusAnalyzer
 			Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
 			int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
 			return numOfEntries;
-		} 
-		catch(IOException e) {
+		}catch(IOException e) {
 			throw new CensusAnalyzerException(e.getMessage(),
-					                         CensusAnalyzerException.ExceptionType.CSV_FILE_PROBLEM);
-		}
-		catch(IllegalStateException e) {
+					                         	CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM);
+		}catch(IllegalStateException e) {
 			throw new CensusAnalyzerException(e.getMessage(),
                     							CensusAnalyzerException.ExceptionType.UNABLE_TO_PARSE);
 		}
+	}
+	
+	
+	@SuppressWarnings("deprecation")
+	public boolean loadIndiaCensusDataFile(String csvFilePath) throws FileNotFoundException, IOException, CensusAnalyzerException {
+		CSVReader reader=null;
+		try{
+			reader= new CSVReader(new FileReader(csvFilePath),'.');
+			String [] nextLine;
+			while ((nextLine=reader.readNext())!= null)  //returns a boolean value  
+			{  
+				   return true;   
+			}   
+			reader.close(); 
+			
+		}catch(IllegalStateException e) {
+		throw new CensusAnalyzerException(e.getMessage(),
+							CensusAnalyzerException.ExceptionType.UNABLE_TO_PARSE);
+		}	
+		return false;	
 	}
 }
 
