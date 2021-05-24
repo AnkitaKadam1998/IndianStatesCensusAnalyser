22f426f71ec92764a6d6b909d0cd84a6a6d2afd5
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-24 16:20:34 +0530

    ADD:code to test given state codes csv file when correct file type incorrect return custom exception

--- CensusAnalyzerTest.java
+++ CensusAnalyzerTest.java
@@ -7,12 +7,13 @@ import org.junit.rules.ExpectedException;
 
 public class CensusAnalyzerTest 
 {
-	private final String INDIAN_CENSUS_CSV_FILE_PATH = "./IndiaCensus.csv";
+	private final String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\Ankita\\eclipse-workspace\\IndainStateCensusAnalyser\\src\\data\\IndiaCensus.csv";
 	private static final String WRONG_CENSUS_CSV_FILE_PATH = "IndiaCensus.csv";
 	private static final String WRONG_CENSUS_FILE_TYPE_PATH = "./IndiaCensus.pdf";
 	
-	private final String INDIAN_STATE_CODE_CSV_FILE_PATH = "./StateCodes.csv";
+	private final String INDIAN_STATE_CODE_CSV_FILE_PATH = "C:\\Users\\Ankita\\eclipse-workspace\\IndainStateCensusAnalyser\\src\\data\\StateCode.csv";
 	private static final String WRONG_STATE_CODE_CSV_FILE_PATH = "StateCodes.csv";
+	private static final String WRONG_STATE_CODE_FILE_TYPE_PATH = "./StateCodes.pdf";
 	
 	//Indian Census analyzer tests
 	@Test
@@ -77,6 +78,18 @@ public class CensusAnalyzerTest
 				statecodeAnalyZer.loadDataFromCSVFile(WRONG_STATE_CODE_CSV_FILE_PATH);
 			} catch(CensusAnalyzerException e) {
 				Assert.assertEquals(CensusAnalyzerException.ExceptionType.CSV_FILE_PROBLEM, e.type);
+			}
+		}
+		
+		@Test
+		public void given_StateCodesData_WithCorrectFile_ButWrongType_ShoulThrewException() throws IOException{
+			try {
+				CensusAnalyzer statecodeAnalyZer = new CensusAnalyzer();
+				ExpectedException exceptionRule = ExpectedException.none();
+				exceptionRule.expect(IOException.class);
+				statecodeAnalyZer.loadDataFromCSVFile(WRONG_STATE_CODE_FILE_TYPE_PATH);
+			} catch(CensusAnalyzerException e) {
+				Assert.assertEquals(CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
 			}
 		}
 }
